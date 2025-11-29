f34197dbfab595bafcc683dd869a8336a4f37cf6
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-11-28 16:59:11 -0800

    Fix version number

--- Plugin.cs
+++ Plugin.cs
@@ -5,8 +5,8 @@ namespace Gaylatea
 {
     namespace UseLooseLoot
     {
-        [BepInPlugin("com.gaylatea.uselooseloot", "SPT-UseLooseLoot", "1.4.0")]
-        [BepInDependency("com.SPT.core", "3.11.0")]
+        [BepInPlugin("com.gaylatea.uselooseloot", "SPT-UseLooseLoot", "1.5.1")]
+        [BepInDependency("com.SPT.core", "4.0.0")]
         public class Plugin : BaseUnityPlugin
         {
             internal static ManualLogSource logger;
